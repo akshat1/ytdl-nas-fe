6997464daddc3421d38f9d14a6663a6277506dbc
Akshat Sharma <akshat.sh@gmail.com>
2019-10-05 23:53:43 -0700

    Added comments for next steps.

--- server/event-handlers.mjs
+++ server/event-handlers.mjs
@@ -1,6 +1,16 @@
 import * as Events from './events';
 import makeTaskManager from './task-manager';
 
+/*
+When a user connects, we send them the current task list via the ClientBootstrap event.
+When a task is added, we emit the QueueUpdated event on the socket, sending all clients the current queue.
+When a task status changes, we send the latest definition of the task down the wires using TaskStatusChanged.
+TODO:
+  - when a task is added, create a new room (clients will connect to that room when the user selects the corresponding task in the UI).
+  - publish all updates from processOne (for instance, updates from curl/aria etc.) to the said room. the clients will use that to display the process output in the browser.
+  - keep the output for each task in a buffer. send that buffer to the user when the user connects to the correspnding room (basically, a 'bootsrtap client for room' event).
+*/
+
 const taskMan = makeTaskManager({
   processOne: (item) => {
     console.log('process', item);
